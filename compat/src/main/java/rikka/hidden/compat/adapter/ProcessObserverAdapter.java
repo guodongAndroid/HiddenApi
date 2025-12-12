a46afb4911299f483737110f70ebc5a441ccc43e
guodongAndroid <sun33919135@gmail.com>
2025-12-12 14:24:58 +0800

    Bump Gradle, GAP and some dependencies versions

--- compat/src/main/java/rikka/hidden/compat/adapter/ProcessObserverAdapter.java
+++ compat/src/main/java/rikka/hidden/compat/adapter/ProcessObserverAdapter.java
@@ -1,7 +1,10 @@
 package rikka.hidden.compat.adapter;
 
 import android.app.IProcessObserver;
+import android.os.Build;
 import android.os.RemoteException;
+
+import androidx.annotation.RequiresApi;
 
 public class ProcessObserverAdapter extends IProcessObserver.Stub {
 
@@ -15,10 +18,12 @@ public class ProcessObserverAdapter extends IProcessObserver.Stub {
         // no longer exists from API 26
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     public void onForegroundServicesChanged(int pid, int uid, int serviceTypes) throws RemoteException {
         // from Q beta 3
     }
 
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     public void onProcessStarted(int pid, int processUid, int packageUid, String packageName, String processName) throws RemoteException {
         // from android-14.0.0_r50
     }
--- stub/src/main/java/android/app/IProcessObserver.java
+++ stub/src/main/java/android/app/IProcessObserver.java
@@ -1,7 +1,10 @@
 package android.app;
 
 import android.os.Binder;
+import android.os.Build;
 import android.os.RemoteException;
+
+import androidx.annotation.RequiresApi;
 
 public interface IProcessObserver {
 
@@ -13,9 +16,11 @@ public interface IProcessObserver {
     void onProcessStateChanged(int pid, int uid, int procState) throws RemoteException;
 
     // from Q beta 3
+    @RequiresApi(Build.VERSION_CODES.Q)
     void onForegroundServicesChanged(int pid, int uid, int serviceTypes) throws RemoteException;
 
     // from android-14.0.0_r50
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     void onProcessStarted(int pid, int processUid, int packageUid, String packageName, String processName) throws RemoteException;
 
     abstract class Stub extends Binder implements IProcessObserver {
